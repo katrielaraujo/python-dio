b279a2072368db3bc861f81977ec5dc853fd3e29
katriel <katrielaraujo@gmail.com>
2023-10-22 17:59:48 -0300

    desafio poo parte1

---
+++ contaCorrente.py
@@ -0,0 +1,31 @@
+from conta import Conta
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente,limite=500,limite_saque=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saque = limite_saque
+
+    def sacar(self,valor):
+        numero_saque = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+        )
+
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saque >= self.limite_saque
+        
+        if excedeu_limite:
+            print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        elif excedeu_saques:
+            print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        else:
+            return super().sacar(valor)
+        
+        return False
+    
+    def __str__(self):
+        return f"""
+            Agência:\t{self.agencia}
+            C/C:\t\t{self.numero}
+            Titulo:\t{self.cliente.nome}
+        """
